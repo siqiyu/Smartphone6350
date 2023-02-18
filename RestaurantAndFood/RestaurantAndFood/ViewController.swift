11791ea07049404a25121d53384ad53d4ff10d42
siqiyu <957826674@qq.com>
2023-02-17 19:42:40 -0800

    checking in code for custom table view cell

---
+++ RestaurantAndFood/RestaurantAndFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantAndFood
+//
+//  Created by yusiqi on 2/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
